006d099a9e00c368bff414e8a7d36af1357d52c0
lvanness7690 <121961344+lvanness7690@users.noreply.github.com>
2023-12-20 20:25:00 -0500

    updating js files

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,73 +1,62 @@
+// index.js
 document.addEventListener('DOMContentLoaded', () => {
-  // Define all variables at the top
-  let noteForm = document.querySelector('.note-form');
-  let noteTitle = document.querySelector('.note-title');
-  let noteText = document.querySelector('.note-textarea');
-  let saveNoteBtn = document.querySelector('.save-note');
-  let newNoteBtn = document.querySelector('.new-note');
-  let clearBtn = document.querySelector('.clear-btn');
-  let noteList = document.querySelector('.list-container .list-group'); // Assuming there's only one list-group
+  const noteForm = document.querySelector('.note-form');
+  const noteTitle = document.querySelector('.note-title');
+  const noteText = document.querySelector('.note-textarea');
+  const saveNoteBtn = document.querySelector('.save-note');
+  const newNoteBtn = document.querySelector('.new-note');
+  const clearBtn = document.querySelector('.clear-btn');
+  const noteList = document.querySelector('.list-container .list-group');
 
-  // Show an element
-  const show = (elem) => {
-    elem.style.display = 'inline';
+  let activeNote = {};
+
+  const getNotes = () => fetch('/api/notes').then(response => response.json());
+
+  const saveNote = (note) =>
+    fetch('/api/notes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(note),
+    }).then(response => response.json());
+
+  const renderNoteList = (notes) => {
+    // Implementation to render notes goes here
   };
-
-  // Hide an element
-  const hide = (elem) => {
-    elem.style.display = 'none';
-  };
-
-  // ... (other helper functions like getNotes, saveNote, deleteNote)
 
   const handleNoteSave = () => {
-    console.log('Attempting to save note.'); // Debug log
-    // Add a check to prevent empty notes from being saved
-    if (!noteTitle.value.trim() || !noteText.value.trim()) {
-      alert("Can't save an empty note.");
-      return;
-    }
-
     const newNote = {
       title: noteTitle.value,
-      text: noteText.value
+      text: noteText.value,
     };
-
-    saveNote(newNote).then(() => {
-      getAndRenderNotes();
-      renderActiveNote();
-    }).catch(error => {
-      console.error('Failed to save the note:', error);
+    saveNote(newNote).then(addNote => {
+      activeNote = {};
+      renderNoteList([...notes, addNote]);
+      noteTitle.value = '';
+      noteText.value = '';
     });
   };
 
+  const handleNoteView = (e) => {
+    // Implementation to view a note goes here
+  };
+
   const handleNewNoteView = () => {
-    console.log('New note view triggered.'); // Debug log
-    // Logic to handle creating a new note view
+    activeNote = {};
     noteTitle.value = '';
     noteText.value = '';
-    noteTitle.focus(); // Focus on the new title input
-    activeNote = {};
-    show(clearBtn);
   };
 
   const handleClearForm = () => {
-    console.log('Clear form triggered.'); // Debug log
-    noteTitle.value = '';
-    noteText.value = '';
-    activeNote = {};
-    hide(saveNoteBtn);
-    hide(newNoteBtn);
+    handleNewNoteView();
   };
 
-  // ... (other event handlers)
+  saveNoteBtn.addEventListener('click', handleNoteSave);
+  newNoteBtn.addEventListener('click', handleNewNoteView);
+  clearBtn.addEventListener('click', handleClearForm);
 
-  if (window.location.pathname === '/notes') {
-    saveNoteBtn.addEventListener('click', handleNoteSave);
-    newNoteBtn.addEventListener('click', handleNewNoteView);
-    clearBtn.addEventListener('click', handleClearForm);
-    noteForm.addEventListener('input', handleRenderBtns);
-  }
-
-  // ... (rest of your code, including getAndRenderNotes call)
+  getNotes().then(notes => {
+    renderNoteList(notes);
+  });
 });
--- server.js
+++ server.js
@@ -1,21 +1,43 @@
+// server.js
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Placeholder for notes (in a real app, you would use a database)
+let notes = [];
 
-// Route to serve the notes.html file
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-// Route for other requests, serving index.html by default
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = Date.now(); // Assign a unique ID to each note
+  notes.push(newNote);
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id);
+  notes = notes.filter(note => note.id !== noteId);
+  res.json({ message: 'Deleted' });
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });

c4b490de952ca6f93ab0d7cc21eb65c93e95ea69
lvanness7690 <121961344+lvanness7690@users.noreply.github.com>
2023-12-20 20:39:28 -0500

    trying index.js fix

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,40 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let noteForm = document.querySelector('.note-form');
   let noteTitle = document.querySelector('.note-title');
   let noteText = document.querySelector('.note-textarea');
   let saveNoteBtn = document.querySelector('.save-note');
-  let newNoteBtn = document.querySelector('.new-note');
-  let clearBtn = document.querySelector('.clear-btn');
   let noteList = document.querySelector('.list-container .list-group');
 
-  const show = (elem) => {
-      elem.style.display = 'inline';
+  const loadNotes = () => {
+      const storedNotes = localStorage.getItem('notes');
+      return storedNotes ? JSON.parse(storedNotes) : [];
   };
 
-  const hide = (elem) => {
-      elem.style.display = 'none';
+  const saveNoteToLocal = (newNote) => {
+      const notes = loadNotes();
+      notes.push(newNote);
+      localStorage.setItem('notes', JSON.stringify(notes));
   };
 
-  const getNotes = () => 
-      fetch('/api/notes', {
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-      }).then(response => response.json());
-
-  const saveNote = (note) =>
-      fetch('/api/notes', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(note)
-      }).then(response => response.json());
+  const renderNoteList = (notes) => {
+      noteList.innerHTML = '';
+      notes.forEach((note, index) => {
+          const noteItem = document.createElement('li');
+          noteItem.classList.add('list-group-item');
+          noteItem.textContent = note.title;
+          noteList.appendChild(noteItem);
+      });
+  };
 
   const handleNoteSave = () => {
       if (!noteTitle.value.trim() || !noteText.value.trim()) {
-          alert("Please fill in both title and text for the note.");
+          alert("Both title and text are required for the note.");
           return;
       }
 
@@ -43,53 +36,13 @@ document.addEventListener('DOMContentLoaded', () => {
           text: noteText.value
       };
 
-      saveNote(newNote).then(() => {
-          getAndRenderNotes();
-          noteTitle.value = '';
-          noteText.value = '';
-          hide(saveNoteBtn);
-          hide(clearBtn);
-      }).catch(error => {
-          console.error('Error saving note:', error);
-          alert('There was an error saving the note.');
-      });
-  };
-
-  const handleNewNoteView = () => {
+      saveNoteToLocal(newNote);
+      renderNoteList(loadNotes());
       noteTitle.value = '';
       noteText.value = '';
-      activeNote = {};
-      show(clearBtn);
-  };
-
-  const handleClearForm = () => {
-      noteTitle.value = '';
-      noteText.value = '';
-      hide(saveNoteBtn);
-      hide(clearBtn);
-  };
-
-  const renderNoteList = (notes) => {
-      // Implement the logic to render the list of notes
-  };
-
-  const getAndRenderNotes = () => {
-      getNotes().then(notes => {
-          renderNoteList(notes);
-      });
   };
 
   saveNoteBtn.addEventListener('click', handleNoteSave);
-  newNoteBtn.addEventListener('click', handleNewNoteView);
-  clearBtn.addEventListener('click', handleClearForm);
-  noteForm.addEventListener('input', () => {
-      if (noteTitle.value.trim() || noteText.value.trim()) {
-          show(saveNoteBtn);
-          show(clearBtn);
-      } else {
-          hide(saveNoteBtn);
-      }
-  });
 
-  getAndRenderNotes();
+  renderNoteList(loadNotes());
 });

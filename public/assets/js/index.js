dc389d48ce75ba7797267a5a6ec18172e13e0608
lvanness7690 <121961344+lvanness7690@users.noreply.github.com>
2023-12-20 20:32:04 -0500

    updating index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,62 +1,95 @@
-// index.js
 document.addEventListener('DOMContentLoaded', () => {
-  const noteForm = document.querySelector('.note-form');
-  const noteTitle = document.querySelector('.note-title');
-  const noteText = document.querySelector('.note-textarea');
-  const saveNoteBtn = document.querySelector('.save-note');
-  const newNoteBtn = document.querySelector('.new-note');
-  const clearBtn = document.querySelector('.clear-btn');
-  const noteList = document.querySelector('.list-container .list-group');
+  let noteForm = document.querySelector('.note-form');
+  let noteTitle = document.querySelector('.note-title');
+  let noteText = document.querySelector('.note-textarea');
+  let saveNoteBtn = document.querySelector('.save-note');
+  let newNoteBtn = document.querySelector('.new-note');
+  let clearBtn = document.querySelector('.clear-btn');
+  let noteList = document.querySelector('.list-container .list-group');
 
-  let activeNote = {};
+  const show = (elem) => {
+      elem.style.display = 'inline';
+  };
 
-  const getNotes = () => fetch('/api/notes').then(response => response.json());
+  const hide = (elem) => {
+      elem.style.display = 'none';
+  };
+
+  const getNotes = () => 
+      fetch('/api/notes', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+      }).then(response => response.json());
 
   const saveNote = (note) =>
-    fetch('/api/notes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(note),
-    }).then(response => response.json());
-
-  const renderNoteList = (notes) => {
-    // Implementation to render notes goes here
-  };
+      fetch('/api/notes', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(note)
+      }).then(response => response.json());
 
   const handleNoteSave = () => {
-    const newNote = {
-      title: noteTitle.value,
-      text: noteText.value,
-    };
-    saveNote(newNote).then(addNote => {
-      activeNote = {};
-      renderNoteList([...notes, addNote]);
-      noteTitle.value = '';
-      noteText.value = '';
-    });
-  };
+      if (!noteTitle.value.trim() || !noteText.value.trim()) {
+          alert("Please fill in both title and text for the note.");
+          return;
+      }
 
-  const handleNoteView = (e) => {
-    // Implementation to view a note goes here
+      const newNote = {
+          title: noteTitle.value,
+          text: noteText.value
+      };
+
+      saveNote(newNote).then(() => {
+          getAndRenderNotes();
+          noteTitle.value = '';
+          noteText.value = '';
+          hide(saveNoteBtn);
+          hide(clearBtn);
+      }).catch(error => {
+          console.error('Error saving note:', error);
+          alert('There was an error saving the note.');
+      });
   };
 
   const handleNewNoteView = () => {
-    activeNote = {};
-    noteTitle.value = '';
-    noteText.value = '';
+      noteTitle.value = '';
+      noteText.value = '';
+      activeNote = {};
+      show(clearBtn);
   };
 
   const handleClearForm = () => {
-    handleNewNoteView();
+      noteTitle.value = '';
+      noteText.value = '';
+      hide(saveNoteBtn);
+      hide(clearBtn);
+  };
+
+  const renderNoteList = (notes) => {
+      // Implement the logic to render the list of notes
+  };
+
+  const getAndRenderNotes = () => {
+      getNotes().then(notes => {
+          renderNoteList(notes);
+      });
   };
 
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   clearBtn.addEventListener('click', handleClearForm);
-
-  getNotes().then(notes => {
-    renderNoteList(notes);
+  noteForm.addEventListener('input', () => {
+      if (noteTitle.value.trim() || noteText.value.trim()) {
+          show(saveNoteBtn);
+          show(clearBtn);
+      } else {
+          hide(saveNoteBtn);
+      }
   });
+
+  getAndRenderNotes();
 });
